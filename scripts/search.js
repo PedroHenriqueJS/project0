6d95e6014eec4516f513ba40755da128119b1f9b
pedrohenriquejs <pedrohjs2013@gmail.com>
2024-05-01 01:28:53 -0300

    alterei scripts e fiz ajustes na mediaquery

--- scripts/search.js
+++ scripts/search.js
@@ -1,9 +1,13 @@
-function checkInput() {
-    var searchInput = document.getElementById('searchInput').value.trim();
-    if (searchInput === '') {
-        document.getElementById('searchBtn').disabled = true;
-        document.getElementById('luckyBtn').disabled = true;
-        return false; 
+const searchInput = document.getElementById('searchInput');
+const searchBtn = document.getElementById('searchBtn');
+const luckyBtn = document.getElementById('luckyBtn');
+
+searchInput.addEventListener('input', function() {
+    if (this.value.trim() === '') {
+        searchBtn.disabled = true;
+        luckyBtn.disabled = true;
+    } else {
+        searchBtn.disabled = false;
+        luckyBtn.disabled = false;
     }
-    return true; 
-}
+});
